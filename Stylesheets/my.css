1c708ce6e928c8fa107f394bd20f187ccf55a1ca
tianshifree <354674557@qq.com>
2016-07-21 01:11:57 +0800

    Create my.css

---
+++ Stylesheets/my.css
@@ -0,0 +1,58 @@
+*{margin:0;border:0;padding:0;font-size:16px;}
+body{}
+.header{min-width:1480px;height:30px;background:#BDC6BD;}
+.main{width:1060px;height:auto;margin:0 auto;}
+.m-body{position:relative;float:left;width:960px;height:auto;background-color:#FBF5E9;}/*参考：A4纸张大小210×297毫米，设定的分辨率是150像素/英寸时,A4纸的尺寸的图像的像素是1240×1754*/
+p{text-indent:2em;}
+h1{text-align:center;}
+a:link,a:visited{text-decoration:none;color:black;}
+a:hover,a:active{font-size:17px;text-decoration:underline;color:red;}
+
+
+.upper{width:500px;height:80px;line-height:80px;margin:0 auto;}
+
+.info{width:960px;height:300px;border-bottom:3px solid blue;position:relative;}
+#details{width:750px;height:280px;float:left;margin-left:10px;}
+#details ul{float:left;width:240px;height:260px;padding-top:25px;margin-left:5px;list-style:none;}
+#details ul li{width:250px;height:40px;line-height:40px;}
+#photo{width:190px;height:290px;float:right;margin:10px 10px 0px 0px;}
+#photo img{width:100%;}
+#weixin{width:150px;position:absolute;float:left;left:570px;top:115px;padding:5px;}
+#weixin img{width:150px;}
+
+.experience{width:940px;height:auto;border-bottom:3px solid blue;}
+.experience h2{padding:20px 0px 20px 20px;}
+.experience .box1{width:900px;height:160px;padding-left: 20px;}
+.experience .box1 h3{height:40px;line-height:50px;}
+.experience .box1 p{height:35px;line-height:35px;}
+
+.skills{width:940px;height:400px;border-bottom:3px solid blue;}
+.skills h2{padding:20px 0px 0px 20px;}
+.skills ol{list-style-type:decimal;padding:20px 0px 0px 50px;}
+.skills ol li{height:35px;line-height:35px;}
+
+.hobbits{width:940px;height:400px;border-bottom:3px solid blue;}
+.hobbits h2{padding:20px 0px 0px 20px;}
+.hobbits ol{list-style-type:decimal;padding:20px 0px 0px 50px;}
+.hobbits ol li{height:35px;line-height:35px;}
+
+.SelfRecom{width:940px;height:auto;border-bottom:3px solid blue;}
+.SelfRecom h2{padding:20px 0px 0px 20px;}
+.SelfRecom p{letter-spacing:1px;line-height:25px;padding:20px 0px 0px 20px;}
+.SelfRecom #thanks{padding:20px 0px 0px 20px;}
+.SelfRecom #thanks p{padding:0px 0px 0px 40px;}
+.SelfRecom .sign{text-indent:80%;text-align:center;padding:0px 0px 0px 20px;}
+
+.m-right{width:110px;background-color:white;position:fixed;left:50%;margin:220px 0px 0px 430px;visibility:hidden;}
+.m-right ul{width:75px;background-color:white;display:block;}
+.m-right ul li{width:75px;list-style-type:none;margin:10px;float:left;}
+.clock{background-color:#f1f1f1;margin:0px 0px 0px 430px;position:fixed;left:50%;}
+
+.m-footer{width:560px;height;50px;margin:0 auto;text-align:center;background-color:red;}
+
+.h-left #time{width:300px;line-height:30px;margin-left:20px;}
+.h-left{width:400px;float:left;}
+.h-right div{float:right;width:100px;height:30px;line-height:30px;background:#CC9;}
+.h-right ul{clear:both;float:right;list-style:none;display:relative;width:100px;text-align:center;visibility:hidden;background-color:#CC9;}
+
+#tests{position:fixed;left:200px;top:200px;font-size:33px;color:red;font-weight:bold;}
